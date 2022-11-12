f510f3f646fe70c23aafe4fbde8919ebf97c8afd
Bianca_ <86985111+biancafrancini@users.noreply.github.com>
2022-11-12 18:46:55 +0000

    added Exercise_27 JS

---
+++ Exercise_27_Rest_spread_syntax/exercise-2.js
@@ -0,0 +1,26 @@
+/**const person = {
+  id: 1,
+  firstName: 'Mario',
+  lastName: 'Rossi',
+  age: 25
+};
+
+const id = person.id;
+const personInfo = {
+  firstName: person.firstName,
+  lastName: person.lastName,
+  age: person.age
+};
+
+console.log(id, personInfo);*/
+
+const person = {
+  id: 1,
+  firstName: "Mario",
+  lastName: "Rossi",
+  age: 25,
+};
+
+const { id, ...rest } = person;
+
+console.log(id, rest);
